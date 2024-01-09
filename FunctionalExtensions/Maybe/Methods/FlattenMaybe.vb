6df1f3eb6933197a324f56b23e1409516c01985d
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-09 23:00:39 +0100

    Added Flatten for both Result and Maybe classes

---
+++ FunctionalExtensions/Maybe/Methods/FlattenMaybe.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Functional
+    Public Module FlattenMaybe
+        <Extension()>
+        Public Function Flatten(Of T)(opt As Maybe(Of Maybe(Of T))) As Maybe(Of T)
+            If opt.IsNone() Then
+                Return Maybe(Of T).None
+            End If
+
+            Return opt.Unwrap()
+        End Function
+    End Module
+End Namespace
---
+++ FunctionalExtensionsTest/Maybe/Methods/FlattenMaybeTest.vb
@@ -0,0 +1,54 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class FlattenMaybeTest
+
+    <TestMethod>
+    Public Sub FlattensSingleLayerSome()
+        Dim expected = Maybe(Of Maybe(Of Integer)).
+            Some(Maybe(Of Integer).
+            Some(1))
+        Dim sut = Maybe(Of Maybe(Of Maybe(Of Integer))).
+            Some(Maybe(Of Maybe(Of Integer)).
+            Some(Maybe(Of Integer).
+            Some(1)))
+        Assert.AreEqual(
+            expected,
+            sut.Flatten()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub FlattensDoubleLayerSome()
+        Dim expected = Maybe(Of Integer).Some(1)
+        Dim sut = Maybe(Of Maybe(Of Maybe(Of Integer))).
+            Some(Maybe(Of Maybe(Of Integer)).
+            Some(Maybe(Of Integer).
+            Some(1)))
+        Assert.AreEqual(
+            expected,
+            sut.Flatten().Flatten()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub FlattensSingleLayerNone()
+        Dim expected = Maybe(Of Maybe(Of Integer)).None()
+        Dim sut = Maybe(Of Maybe(Of Maybe(Of Integer))).None()
+        Assert.AreEqual(
+            expected,
+            sut.Flatten()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub FlattensDoubleLayerNone()
+        Dim expected = Maybe(Of Integer).None()
+        Dim sut = Maybe(Of Maybe(Of Maybe(Of Integer))).None()
+        Assert.AreEqual(
+            expected,
+            sut.Flatten().Flatten()
+        )
+    End Sub
+End Class
