c505be0e729ea883b24146177045ecc46f6ab6f2
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-15 21:31:49 +0100

    Added OnOk method for Result

---
+++ FunctionalExtensions/Result/Methods/OnOkResult.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Functional
+    Public Module OnOkResult
+        <Extension()>
+        Public Function OnOk(Of T, E)(res As Result(Of T, E), action As Action(Of T)) As Result(Of T, E)
+            If res.IsOk() Then
+                action(res.Unwrap())
+            End If
+
+            Return res
+        End Function
+    End Module
+End Namespace
---
+++ FunctionalExtensionsTest/Result/Methods/OnOkResultTest.vb
@@ -0,0 +1,40 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class OnOkResultTest
+    <TestMethod>
+    Public Sub OnOkDoesCallActionOnOk()
+        Dim _global = 0
+        Dim testValue = 5
+        Dim expected = _global + testValue
+        Dim sut = Result(Of Integer, String).Ok(-1)
+
+        sut.OnOk(Sub(err)
+                     _global += testValue
+                 End Sub)
+
+        Assert.AreEqual(
+            expected,
+            _global
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub OnOkDoesNotCallActionOnErr()
+        Dim _global = 0
+        Dim testValue = 5
+        Dim expected = _global
+        Dim sut = Result(Of Integer, String).Err("What!")
+
+        sut.OnOk(Sub(err)
+                     _global += testValue
+                 End Sub)
+
+        Assert.AreEqual(
+            expected,
+            _global
+        )
+    End Sub
+
+End Class
