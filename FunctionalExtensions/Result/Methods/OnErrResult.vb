c71b7ee278743624031e4724d18defcc932ce6cd
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-15 21:29:04 +0100

    Added the method OnErr for result

--- FunctionalExtensions/Result/Methods/OnErrResult.vb
+++ FunctionalExtensions/Result/Methods/OnErrResult.vb
@@ -1,3 +1,14 @@
-﻿Public Class OnErr
+﻿Imports System.Runtime.CompilerServices
 
-End Class
+Namespace Functional
+    Public Module OnErrResult
+        <Extension()>
+        Public Function OnErr(Of T, E)(res As Result(Of T, E), action As Action(Of E)) As Result(Of T, E)
+            If res.IsErr() Then
+                action(res.Err)
+            End If
+
+            Return res
+        End Function
+    End Module
+End Namespace
---
+++ FunctionalExtensionsTest/Result/Methods/OnErrResultTest.vb
@@ -0,0 +1,40 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class OnErrResultTest
+    <TestMethod>
+    Public Sub OnErrDoesNotCallActionOnOk()
+        Dim _global = 0
+        Dim testValue = 5
+        Dim expected = _global
+        Dim sut = Result(Of Integer, String).Ok(-1)
+
+        sut.OnErr(Sub(err)
+                      _global += testValue
+                  End Sub)
+
+        Assert.AreEqual(
+            expected,
+            _global
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub OnErrDoesCallActionOnErr()
+        Dim _global = 0
+        Dim testValue = 5
+        Dim expected = _global + testValue
+        Dim sut = Result(Of Integer, String).Err("What!")
+
+        sut.OnErr(Sub(err)
+                      _global += testValue
+                  End Sub)
+
+        Assert.AreEqual(
+            expected,
+            _global
+        )
+    End Sub
+
+End Class
